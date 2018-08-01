1ce27de8b2e0cd97d8b0e9fdaa7f8e87c6b44ced
Logan <lczernel@gmail.com>
2018-08-01 22:13:03 +0100

    First commit! Welcome to StickWor|d...

---
+++ Main.java
@@ -0,0 +1,13 @@
+/*
+Logan Czernel
+This is a very simple game that involves printing characters that look like characters to simulate very low level
+game graphics. The main purpose of the project was to give me a more hands on learning experience as I learn Java.
+ */
+
+class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to StickWor|d...")
+    }
+
+}
