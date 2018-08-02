cf3dc31a0c6ac09a46f3f7874cf194145be8e0e2
Logan <lczernel@gmail.com>
2018-08-02 22:35:12 +0100

    f

--- Main.java
+++ Main.java
@@ -4,10 +4,54 @@ This is a very simple game that involves printing characters that look like char
 game graphics. The main purpose of the project was to give me a more hands on learning experience as I learn Java.
  */
 
+
+package SimpleJavaGame;
+
+
+class Boards {
+    String baseBoard = "" +
+            "|---------------------------|\n" +
+            "|                           |\n" +
+            "|                           |\n" +
+            "|             %s             |\n" +
+            "|            %s%s%s            |\n" +
+            "|            %s%s%s            |\n" +
+            "|____________%s%s%s____________|\n" +
+            "|              %s            |\n" +
+            "|              %s            |\n" +
+            "|              %s            |\n" +
+            "|              %s            |\n" +
+            "|---------------------------|\n";
+
+    public String getPlayerStand() {
+        String output = String.format(this.baseBoard, " ", " ","0", " ", "/", "O", "\\", "/", "_", "\\",
+                " ", " ", " ", " ");
+        return output;
+    }
+
+    public String getPlayerRunOne() {
+        String output = String.format(this.baseBoard, " ", " ","0", " ", "-", "O", "\\", "/", "_", ")",
+                " ", " ", " ", " ");
+        return output;
+    }
+
+    public String getPlayerRunTwo() {
+        String output = String.format(this.baseBoard, " ", " ","0", " ", "/", "O", "-", "(", "_", "\\",
+                " ", " ", " ", " ");
+        return output;
+    }
+}
+
 class Main {
 
     public static void main(String[] args) {
-        System.out.println("Welcome to StickWor|d...")
+        System.out.println("\n\nWelcome to StickWor|d...\n\nPress 'SPACE' to start!\n\n");
+        Object newBoard = new Boards();
+        System.out.println(((Boards) newBoard).getPlayerStand());
+        System.out.println(((Boards) newBoard).getPlayerRunOne());
+        System.out.println(((Boards) newBoard).getPlayerStand());
+        System.out.println(((Boards) newBoard).getPlayerRunTwo());
+        System.out.println(((Boards) newBoard).getPlayerStand());
     }
 
 }
