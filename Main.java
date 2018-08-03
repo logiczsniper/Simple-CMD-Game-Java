834879f35c025586b8040dfbeea54622cd0d12cb
Logan <lczernel@gmail.com>
2018-08-03 22:56:07 +0100

    Boards class can now create any game board required by the game loop,
    including spike positions. Main class now has a method to house the game
    loop, and the main method now waits for the user to press SPACE and then
    ENTER to call the runGame method.

--- Main.java
+++ Main.java
@@ -6,9 +6,15 @@ game graphics. The main purpose of the project was to give me a more hands on le
 
 
 package SimpleJavaGame;
+import java.util.Scanner;
 
 
 class Boards {
+    String spikeZero = "_";
+    String spikeOne = " ";
+    String spikeTwo = " ";
+    String spikeThree = " ";
+    String spikeFour = " ";
     String baseBoard = "" +
             "|---------------------------|\n" +
             "|                           |\n" +
@@ -17,41 +23,111 @@ class Boards {
             "|            %s%s%s            |\n" +
             "|            %s%s%s            |\n" +
             "|____________%s%s%s____________|\n" +
-            "|              %s            |\n" +
-            "|              %s            |\n" +
-            "|              %s            |\n" +
-            "|              %s            |\n" +
+            "|             %s             |\n" +
+            "|             %s             |\n" +
+            "|             %s             |\n" +
+            "|             %s             |\n" +
             "|---------------------------|\n";
 
-    public String getPlayerStand() {
-        String output = String.format(this.baseBoard, " ", " ","0", " ", "/", "O", "\\", "/", "_", "\\",
-                " ", " ", " ", " ");
+    public void resetSpikes() {
+        this.spikeZero = "_";
+        this.spikeOne = " ";
+        this.spikeTwo = " ";
+        this.spikeThree = " ";
+        this.spikeFour = " ";
+    }
+
+    public String getPlayerStand(int spikePos) {
+
+        prepareSpikes(spikePos);
+
+        String output = String.format(this.baseBoard, " ", " ", "0", " ", "/", "O", "\\", "/", this.spikeZero, "\\",
+                this.spikeOne, this.spikeTwo, this.spikeThree, this.spikeFour);
         return output;
     }
 
-    public String getPlayerRunOne() {
-        String output = String.format(this.baseBoard, " ", " ","0", " ", "-", "O", "\\", "/", "_", ")",
-                " ", " ", " ", " ");
+    public String getPlayerRunOne(int spikePos) {
+
+        prepareSpikes(spikePos);
+
+        String output = String.format(this.baseBoard, " ", " ", "0", " ", "-", "O", "\\", "/", this.spikeZero, ")",
+                this.spikeOne, this.spikeTwo, this.spikeThree, this.spikeFour);
         return output;
     }
 
-    public String getPlayerRunTwo() {
-        String output = String.format(this.baseBoard, " ", " ","0", " ", "/", "O", "-", "(", "_", "\\",
-                " ", " ", " ", " ");
+    public String getPlayerRunTwo(int spikePos) {
+
+        prepareSpikes(spikePos);
+
+        String output = String.format(this.baseBoard, " ", " ", "0", " ", "/", "O", "-", "(", this.spikeZero, "\\",
+                this.spikeOne, this.spikeTwo, this.spikeThree, this.spikeFour);
         return output;
+    }
+
+    public String getPlayerUp(int spikePos) {
+
+        prepareSpikes(spikePos);
+
+        String output = String.format(this.baseBoard, "0", "\\", "O", "/", "<", " ", ">", "_", this.spikeZero, "_",
+                this.spikeOne, this.spikeTwo, this.spikeThree, this.spikeFour);
+        return output;
+    }
+
+    private void prepareSpikes(int spikePos) {
+
+        resetSpikes();
+
+        if (spikePos == 0) {
+            this.spikeZero = "^";
+        } else if (spikePos == 1) {
+            this.spikeOne = "^";
+        } else if (spikePos == 2) {
+            this.spikeTwo = "^";
+        } else if (spikePos == 3) {
+            this.spikeThree = "^";
+        } else if (spikePos == 4) {
+            this.spikeFour = "^";
+        }
     }
 }
 
 class Main {
 
-    public static void main(String[] args) {
-        System.out.println("\n\nWelcome to StickWor|d...\n\nPress 'SPACE' to start!\n\n");
+    public static void runGame() {
+
+        boolean gameEnd = false;
         Object newBoard = new Boards();
-        System.out.println(((Boards) newBoard).getPlayerStand());
-        System.out.println(((Boards) newBoard).getPlayerRunOne());
-        System.out.println(((Boards) newBoard).getPlayerStand());
-        System.out.println(((Boards) newBoard).getPlayerRunTwo());
-        System.out.println(((Boards) newBoard).getPlayerStand());
+
+        while (gameEnd == false) {
+
+            try {
+                Thread.sleep(1500);
+            }
+            catch (InterruptedException ex) {
+                Thread.currentThread().interrupt();
+            }
+
+            // TODO: ignoring player jumps and damage, get a loop running using the four following game boards.
+
+            System.out.println(((Boards) newBoard).getPlayerStand(4));
+            System.out.println(((Boards) newBoard).getPlayerRunOne(3));
+            System.out.println(((Boards) newBoard).getPlayerStand(2));
+            System.out.println(((Boards) newBoard).getPlayerRunTwo(1));
+
+            gameEnd = true;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("\n\nWelcome to StickWor|d...\n\nPress 'SPACE' followed by 'ENTER' to start!\n\n");
+        Scanner user_input = new Scanner(System.in);
+        String input = user_input.nextLine();
+        String search = " ";
+
+        if (input.indexOf(search) != -1) {
+            runGame();
+        }
     }
 
 }
